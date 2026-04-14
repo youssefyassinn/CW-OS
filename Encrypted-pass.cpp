ccdd8310125e2236e683af0f227607db5153b4f8
youssefyassinn <YE2300329@tkh.edu.eg>
2026-04-14 21:19:49 +0000

    my commit message

--- Encrypted-pass.cpp
+++ Encrypted-pass.cpp
@@ -2,28 +2,32 @@
 #include <openssl/des.h>
 using namespace std;
 
-// this function encrypts the password using DES 25 times
+// this function does the encryption
 string enc(string p, int s) {
     DES_cblock k = {0}, b = {0}, r;
     DES_key_schedule sch;
 
-    // put password into key 
+    // put password into key (only first 8 chars)
     for (int i = 0; i < 8 && i < p.length(); i++)
         k[i] = p[i];
 
+    // setup DES
     DES_set_key_unchecked(&k, &sch);
 
-    // using salt to make starting block
+    // use salt to make starting block
     b[0] = s / 256;
     b[1] = s % 256;
 
-    // repeat encryption 25 times
+    // repeat DES 25 times 
     for (int i = 0; i < 25; i++) {
         DES_ecb_encrypt(&b, &r, &sch, DES_ENCRYPT);
-        b = r;
+
+        // copy result back (arrays cant be assigned directly)
+        for (int j = 0; j < 8; j++)
+            b[j] = r[j];
     }
 
-    // making final string (salt:hash)
+    // make final string 
     string out = to_string(s) + ":";
     for (int i = 0; i < 8; i++)
         out += to_string((int)r[i]);
@@ -32,11 +36,14 @@ string enc(string p, int s) {
 }
 
 int main() {
+
+    // some passwords i chose
     string myPass[10] = {
-        "money1","meen","ana3","dead4","good5",
+        "money1","hello2","main3","insane4","true5",
         "test1","test2","hello1","abc123","pass99"
     };
 
+    // salts (just random numbers)
     int mySalt[10] = {
         111,222,333,444,555,
         666,777,888,999,1234
@@ -44,21 +51,23 @@ int main() {
 
     cout << "Encrypted passwords:\n";
 
-    // printing encrypted passwords
+    // encrypt all passwords
     for (int i = 0; i < 10; i++) {
         cout << myPass[i] << " -> " << enc(myPass[i], mySalt[i]) << endl;
     }
 
-    // checking part
+    // now checking if password is correct
     cout << "\nChecking password:\n";
 
     string saved = enc("money1", 111);
 
+    // correct password test
     if (enc("money1", 111) == saved)
         cout << "money1 correct\n";
     else
         cout << "money1 wrong\n";
 
+    // wrong password test
     if (enc("wrong", 111) == saved)
         cout << "wrong correct\n";
     else
